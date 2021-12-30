86584033cf0ce93357fd91ad635d58b040da3830
Richard Mands <richard@devwell.org>
2021-12-30 20:02:51 +0000

    Instantiate client with appIndex via FE

--- src/Kin.tsx
+++ src/Kin.tsx
@@ -2,15 +2,24 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './Kin.scss'
 
+interface Input{
+  name: string;
+  value: string;
+  options?: string[];
+  type?: string;
+  onChange: (arg: string) => void;
+}
 interface KinActionProps{
     title: string;
     actionName: string;
     action: () => void;
+    inputs?: Input[];
 }
 function KinAction({
     title,
     actionName,
     action,
+    inputs ,
 }:KinActionProps) {
     return (
       <>
@@ -18,6 +27,21 @@ function KinAction({
           {title}
         </div>
         <div className="Kin-action">
+          {inputs?.length ? inputs.map(({
+          type = 'text', name, value, options, onChange,
+          }) => (
+            <div key={name} className="Kin-action-input-container">
+              <label htmlFor={`Kin-action-for-${name}`} className="Kin-action-label">
+                {name}
+              </label>
+              {options?.length ? (
+                <select id={`Kin-action-for-${name}`} className="Kin-action-input">
+                  {options.map((option) => <option key={option} value={option}>{option}</option>)}
+                </select>
+) : (<input onChange={(event) => onChange(event.target.value)} type={type} value={value} id={`Kin-action-for-${name}`} />)}
+
+            </div>
+          )) : null}
           <button type="button" className="Kin-action-button" onClick={action}>
             {actionName}
           </button>
@@ -30,87 +54,147 @@ interface Response{
     status: number;
 }
 
-interface HandleCreateAccount{
-    onSuccess: (arg: string) => void;
-    onFailure: (arg: any) => void;
-}
-
-async function handleCreateAccount({ onSuccess, onFailure }:HandleCreateAccount) {
-    try {
-        const baseUrl = process.env.REACT_APP_SERVER_URL
-        if(!baseUrl) throw new Error("No URL");
-
-        const url = `${baseUrl}/account`
-        const response:Response = await axios.post(url)
-        onSuccess(response.data)
-    } catch (error) {
-        onFailure(error)
-    }
+interface ClientReponse {
+  status: number;
+  data: {
+    appIndex?: number
+  }
 }
 
 interface CheckServerRunning{
-    onSuccess: (arg: number) => void;
+    onSuccess: (arg: ClientReponse) => void;
     onFailure: (arg: any) => void;
-}
-async function checkServerRunning({ onSuccess, onFailure }:CheckServerRunning) {
+  }
+  async function checkServerRunning({ onSuccess, onFailure }:CheckServerRunning) {
     try {
-        const baseUrl = process.env.REACT_APP_SERVER_URL
-        if(!baseUrl) throw new Error("No URL");
+      const baseUrl = process.env.REACT_APP_SERVER_URL
+      if(!baseUrl) throw new Error("No URL");
 
-        const url = `${baseUrl}/healthcheck`
-        const response:Response = await axios.get(url)
-        onSuccess(response.status)
+      const url = `${baseUrl}/healthcheck`
+      const response:ClientReponse = await axios.get(url)
+      onSuccess(response)
     } catch (error) {
-        onFailure(error)
+      onFailure(error)
     }
-}
+  }
+interface HandleSetupKinClient{
+    onSuccess: () => void;
+    onFailure: (arg: any) => void;
+    kinEnvironment: string;
+    appIndex: string;
+  }
+  async function handleSetupKinClient({
+    onSuccess, onFailure, kinEnvironment, appIndex,
+  }:HandleSetupKinClient) {
+    try {
+      const baseUrl = process.env.REACT_APP_SERVER_URL
+      if(!baseUrl) throw new Error("No URL");
+
+      const url = `${baseUrl}/setup?env=${kinEnvironment}&appIndex=${appIndex}`
+      const response:Response = await axios.post(url)
+
+      if(response.status === 201) {
+        onSuccess()
+      } else {
+        throw new Error("No appIndex");
+      }
+    } catch (error) {
+      onFailure(error)
+    }
+  }
+
+  interface HandleCreateAccount{
+      onSuccess: (arg: string) => void;
+      onFailure: (arg: any) => void;
+  }
+
+  async function handleCreateAccount({ onSuccess, onFailure }:HandleCreateAccount) {
+      try {
+          const baseUrl = process.env.REACT_APP_SERVER_URL
+          if(!baseUrl) throw new Error("No URL");
+
+          const url = `${baseUrl}/account`
+          const response:Response = await axios.post(url)
+          onSuccess(response.data)
+      } catch (error) {
+          onFailure(error)
+      }
+  }
 
 function Kin() {
     const [serverRunning, setServerRunning] = useState(false)
+    const [serverAppIndex, setServerAppIndex] = useState(0)
+    const [shouldUpdate, setShouldUpdate] = useState(true)
     useEffect(() => {
+      if(shouldUpdate) {
         checkServerRunning({
-              onSuccess: (response) => {
-                  if(response === 200) {
-                      setServerRunning(true)
-                  } else {
-                      setServerRunning(false)
-                  }
+              onSuccess: ({ status, data }) => {
+                setServerRunning(status === 200)
+                setServerAppIndex(data?.appIndex || 0)
               },
               onFailure: () => setServerRunning(false),
           })
-    }, [])
-    // const [users, setUsers] = useState({})
+
+          setShouldUpdate(false)
+      }
+    }, [shouldUpdate])
+    const [kinEnvironment, setKinEnvironment] = useState('Test')
+    const [appIndex, setAppIndex] = useState('')
+
+    // const [users, setUsers] = useState([])
 
     return (
       <div className="Kin">
         <div className={`Kin-status ${serverRunning ? 'up' : 'down'}`}>
           {serverRunning ? 'Server Running' : 'Server Not Running'}
+          {serverAppIndex ? ` - appIndex ${serverAppIndex}` : ' but Client not instantiated :('}
         </div>
         {serverRunning ? (
           <>
             <KinAction
-              title="Create a Kin Account"
-              actionName="Create"
-              action={() => { handleCreateAccount({
-              onSuccess: (data) => console.log(data),
-              onFailure: (error) => console.log(error),
-          }) }}
+              title="Setup Your Kin Client"
+              actionName="Setup"
+              action={() => { handleSetupKinClient({
+                  onSuccess: () => setShouldUpdate(true),
+                  onFailure: (error) => console.log(error),
+                  kinEnvironment,
+                  appIndex,
+              }) }}
+              inputs={[
+                {
+                  name: 'Environment', value: kinEnvironment, options: ['Test', 'Prod'], onChange: setKinEnvironment,
+                },
+               {
+                  name: 'App Index', value: appIndex, type: 'number', onChange: setAppIndex,
+                }]}
             />
-            <KinAction
-              title="Get Account Balance"
-              actionName="Get"
-              action={() => { console.log('click!') }}
-            />
-            <KinAction
-              title="Request Airdrop"
-              actionName="Request"
-              action={() => { console.log('click!') }}
-            />
-            <KinAction
-              title="Send Kin"
-              actionName="Send"
-              action={() => { console.log('click!') }}
-            />
+            {serverAppIndex ? (
+              <>
+                <KinAction
+                  title="Create a Kin Account"
+                  actionName="Create"
+                  action={() => { handleCreateAccount({
+                  onSuccess: (data) => console.log(data),
+                  onFailure: (error) => console.log(error),
+              }) }}
+                />
+                <KinAction
+                  title="Get Account Balance"
+                  actionName="Get"
+                  action={() => { console.log('click!') }}
+                />
+                <KinAction
+                  title="Request Airdrop"
+                  actionName="Request"
+                  action={() => { console.log('click!') }}
+                />
+                <KinAction
+                  title="Send Kin"
+                  actionName="Send"
+                  action={() => { console.log('click!') }}
+                />
+              </>
+) : null}
           </>
 ) : null}
 
