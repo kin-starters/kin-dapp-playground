a181d69c9d22389c2c7e6e1b81fca9f7546df16f
Richard Mands <richard@devwell.org>
2022-04-05 12:21:04 +0100

    Working Web-SDK

--- src/constants.ts
+++ src/constants.ts
@@ -245,6 +245,6 @@ export const solanaAddresses = {
     kinMint: 'kinXdEcpDQeHPEuQnqmUgtYykqKGVFq6CeVX5iAHJq6',
   },
   Devnet: {
-    kinMint: '',
+    kinMint: 'KinD3xAzuqX3LJbxUG13peGnqjpV5dRcHV8tsdsrbeZ',
   },
 };
